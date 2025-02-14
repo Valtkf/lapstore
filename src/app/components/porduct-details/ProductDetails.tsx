9f9bd1f57dcf499bd264341ff63d1ee76e3037ff
Valtkf <valentin.vgregorio@gmail.com>
2025-02-14 15:59:21 +0100

    💄  F01 : ProductDetails > add styling and dynamic display to ProductDetails component

--- src/app/components/porduct-details/ProductDetails.tsx
+++ src/app/components/porduct-details/ProductDetails.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import { urlForImage } from "@/sanity/lib/image";
+import { useState } from "react";
 
 interface SanityImage {
   _type: string;
@@ -21,22 +22,39 @@ interface Product {
 }
 
 export default function ProductDetails({ product }: { product: Product }) {
+  const [index, setIndex] = useState(0);
   return (
-    <div className="w-[300px]">
-      {product.images &&
-        product.images.map((image: SanityImage, index: number) => (
-          <Image
-            key={index}
-            src={urlForImage(image).url()}
-            width={200}
-            height={200}
-            alt={product.slug.current}
-            style={{ objectFit: "cover" }}
-            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 25vw"
-          />
-        ))}
-      <div>{product.name}</div>
-      <div>{product.price} €</div>
+    <div className="flex justify-center gap-4">
+      <div className="flex justify-center flex-col w-[150px] ml-10 gap-y-4">
+        {product.images &&
+          product.images.map((image: SanityImage, index: number) => (
+            <Image
+              loader={() => urlForImage(product.images[index]).url()}
+              key={index}
+              src={urlForImage(product.images[index]).url()}
+              width={100}
+              height={100}
+              alt={product.slug.current}
+              style={{ objectFit: "cover" }}
+              className="cursor-pointer shadow-lg rounded-xl p-2"
+              onClick={() => setIndex(index)}
+            />
+          ))}
+      </div>
+      <div className="w-[450px]">
+        <Image
+          loader={() => urlForImage(product.images[index]).url()}
+          src={urlForImage(product.images[index]).url()}
+          alt={product.slug.current}
+          width={350}
+          height={350}
+          className="object-cover mx-auto w-full"
+        />
+      </div>
+      <div className="flex flex-col gap-y-4 w-2/5">
+        <div className="text-xl font-bold">{product.name}</div>
+        <div className="text-xl font-bold">{product.price} €</div>
+      </div>
     </div>
   );
 }
